e31cf32c686fa28af9d639b54ef131ad8406dd5f
helenohyeah <ouyang.helen@gmail.com>
2020-11-09 18:33:53 +0000

    Complete exercises 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,12 +3,13 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
 # Use Active Record's create class method multiple times to create 3 stores in the database:
 # Burnaby (annual_revenue of 300000, carries men's and women's apparel)
 # Richmond (annual_revenue of 1260000 carries women's apparel only)
 # Gastown (annual_revenue of 190000 carries men's apparel only)
 # Output (puts) the number of the stores using ActiveRecord's count method, to ensure that there are three stores in the database.
+
+# Your code goes below here ...
 
 store1 = Store.create(
   name: "Burnaby",
@@ -31,4 +32,4 @@ store3 = Store.create(
   womens_apparel: false
 )
 
-puts "Store count: #{Store.count}"
+# puts "Store count: #{Store.count}"
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,17 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+# Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
+# Load the second store from the database and assign it to @store2.
+# Update the first store (@store1) instance in the database. (Change its name or something.)
+
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+# puts "Store 1: #{@store1.inspect}"
+# puts "Store 2: #{@store2.inspect}"
+
+@store3 = Store.find_by(id: 3)
+@store3.name = "Yaletown"
+# puts "Store 3: #{@store3.inspect}"
