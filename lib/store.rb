9184d84268f331a30b097a0129f6b2ed3e6f7fde
helenohyeah <ouyang.helen@gmail.com>
2020-11-09 19:32:59 +0000

    Complete exercise 7

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,17 @@
 class Store < ActiveRecord::Base
+
   has_many :employees
+
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: {
+    greater_than_or_equal_to: 0
+  }
+  validate :validate_selection
+
+  def validate_selection
+    if self.mens_apparel.nil? && self.womens_apparel.nil?
+      errors.add(:self, "Must carry women or mens clothes")
+    end
+  end
+
 end
